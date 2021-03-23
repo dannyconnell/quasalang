b2a5c3ca421ea2f67fc71f413f9bb73513f3a46c
Danny Connell <sendstufftodanny@gmail.com>
2021-03-23 19:32:28 +0000

    Got createCSV working & generate working with parameters

--- commands.js
+++ commands.js
@@ -11,19 +11,19 @@ program
   .description('Generate Quasar i18n language files from a CSV file')
 
 program
-  .command('create-csv')
-  .alias('c')
-  .description('Create a sample CSV file')
-  .action(() => {
-    createCSV()
-  })
-
-program
   .command('generate <csvpath> <outputpath>')
   .alias('g')
   .description('Generate your i18n folder & all language files based on a CSV file')
   .action((csvpath, outputpath) => {
     generate(csvpath, outputpath)
+  })
+
+program
+  .command('create-csv')
+  .alias('c')
+  .description('Create a sample CSV file')
+  .action(() => {
+    createCSV()
   })
 
 program.parse(process.argv)
